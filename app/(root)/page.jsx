3af17f58f7935a4bc3f940e8b49f4bd49a415b96
saman <sam.karimi727@gmail.com>
2025-10-19 03:15:54 +0330

    update 5 files and create 3 files

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,23 +1,15 @@
-import { Accordion } from "radix-ui";
+import {Button} from '@heroui/button'; 
 
 export default function HomePage() {
   return (
  <div>
   <h1 className="text-3xl text-green-500">homepage</h1>
-  <Accordion.Root
-  type="single"
-  defaultValue="item-1"
-  collapsible
-  >
-		<Accordion.Item value="item-1">
-			<Accordion.Header>
-				<Accordion.Trigger >hello</Accordion.Trigger>
-			</Accordion.Header>
-			<Accordion.Content >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quisquam nostrum, unde voluptas eos fugit dolorem doloremque quas, eum iste, numquam ad necessitatibus mollitia libero consequuntur dignissimos minima. Aliquam, porro.
-      </Accordion.Content>
-		</Accordion.Item>
-	</Accordion.Root>
+  <Button
+      className="bg-linear-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+      radius="full"
+    >
+      Button
+    </Button>
  </div>
   );
 }
