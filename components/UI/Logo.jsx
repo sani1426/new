dc2b914a57338be7ce4d52075d39969022403a28
saman <sam.karimi727@gmail.com>
2025-10-19 23:56:12 +0330

    update Logo.jsx

--- components/UI/Logo.jsx
+++ components/UI/Logo.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <Link href="/" className='flex items-center gap-2'>
+    <Link href="/" className='flex items-center gap-2 py-2'>
         <Image src="/images/logo.png" width={50} height={20} className='rounded-2xl logo' alt='logo-image'  />
         <h1 className='text-3xl lg:text-4xl text-nowrap'>Mr.Sam</h1>
     </Link>
