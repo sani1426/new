6e30914841f6f13e884ad209ca17e4a1e53e5f7c
saman <sam.karimi727@gmail.com>
2025-10-22 00:44:13 +0330

    update DropDownMenu.jsx

--- components/shared/DropDownMenu.jsx
+++ components/shared/DropDownMenu.jsx
@@ -1,95 +1,38 @@
+
 import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
+        Dropdown,
+        DropdownTrigger,
+        DropdownMenu,
+        DropdownItem
+      } from "@heroui/dropdown";
+import { Button } from '@heroui/button';
 
-  DropdownItem,
+const DropMenu = () => {
   
-} from "@heroui/dropdown";
-import {User} from "@heroui/user"
-import {Button} from "@heroui/button"
-
-
-
-export default function DropMenu() {
   return (
-    <Dropdown
-      showArrow
-      classNames={{
-        base: "before:bg-default-200", // change arrow background
-        content: "p-0 border-small border-divider bg-background",
-      }}
-      radius="sm"
-    >
-      <DropdownTrigger>
-        <Button disableRipple variant="ghost">
-          Open
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu
-        className="p-3"
-        itemClasses={{
-          base: [
-            "rounded-md",
-            "text-default-500",
-            "transition-opacity",
-            "data-[hover=true]:text-foreground",
-            "data-[hover=true]:bg-default-100",
-            "dark:data-[hover=true]:bg-default-50",
-            "data-[selectable=true]:focus:bg-default-50",
-            "data-[pressed=true]:opacity-70",
-            "data-[focus-visible=true]:ring-default-500",
-          ],
-        }}
-      >
-          <DropdownItem key="profile" isReadOnly className="h-14 gap-2 opacity-100">
-            <User
-              avatarProps={{
-                size: "sm",
-                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-              }}
-              classNames={{
-                name: "text-default-600",
-                description: "text-default-500",
-              }}
-              description="@jrgarciadev"
-              name="Junior Garcia"
-            />
-          </DropdownItem>
-          <DropdownItem key="dashboard">Dashboard</DropdownItem>
-          <DropdownItem key="settings">Settings</DropdownItem>
-          <DropdownItem key="new_project" >
-            New Project
-          </DropdownItem>
-
-          <DropdownItem key="quick_search" >
-            Quick search
-          </DropdownItem>
+        <Dropdown
+        showArrow
+        classNames={{
+          base: "before:bg-default-200", // change arrow background
+          content:
+            "py-1 px-1 border border-default-200 bg-linear-to-br from-white to-default-200 dark:from-default-50 dark:to-black",
+        }}>
+        <DropdownTrigger>
+          <Button variant="bordered">open menu</Button>
+        </DropdownTrigger>
+        <DropdownMenu variant="faded">
+        <DropdownSection title="Actions">
           <DropdownItem
-            key="theme"
-            isReadOnly
-            className="cursor-default"
-            endContent={
-              <select
-                className="z-10 outline-solid outline-transparent w-16 py-0.5 rounded-md text-tiny group-data-[hover=true]:border-default-500 border-small border-default-300 dark:border-default-200 bg-transparent text-default-600"
-                id="theme"
-                name="theme"
-              >
-                <option>System</option>
-                <option>Dark</option>
-                <option>Light</option>
-              </select>
-            }
+            key="new"
+            description="Create a new file"
+            shortcut="⌘N"
           >
-            Theme
+            New file
           </DropdownItem>
-      
-
-       
-          <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-          <DropdownItem key="logout">Log Out</DropdownItem>
-  
-      </DropdownMenu>
-    </Dropdown>
-  );
+          </DropdownSection>
+        </DropdownMenu>
+      </Dropdown>
+  )
 }
+
+export default DropMenu
