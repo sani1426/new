1184584921d0bc262e59c27cffe54778386cfc27
saman <sam.karimi727@gmail.com>
2025-10-21 23:52:52 +0330

    update DropDownMenu.jsx

--- components/shared/DropDownMenu.jsx
+++ components/shared/DropDownMenu.jsx
@@ -9,31 +9,7 @@ import {
 import {User} from "@heroui/user"
 import {Button} from "@heroui/button"
 
-export const PlusIcon = (props) => {
-  return (
-    <svg
-      aria-hidden="true"
-      fill="none"
-      focusable="false"
-      height="1em"
-      role="presentation"
-      viewBox="0 0 24 24"
-      width="1em"
-      {...props}
-    >
-      <g
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={1.5}
-      >
-        <path d="M6 12h12" />
-        <path d="M12 18V6" />
-      </g>
-    </svg>
-  );
-};
+
 
 export default function DropMenu() {
   return (
@@ -51,9 +27,7 @@ export default function DropMenu() {
         </Button>
       </DropdownTrigger>
       <DropdownMenu
-        aria-label="Custom item styles"
         className="p-3"
-        disabledKeys={["profile"]}
         itemClasses={{
           base: [
             "rounded-md",
@@ -84,11 +58,11 @@ export default function DropMenu() {
           </DropdownItem>
           <DropdownItem key="dashboard">Dashboard</DropdownItem>
           <DropdownItem key="settings">Settings</DropdownItem>
-          <DropdownItem key="new_project" endContent={<PlusIcon className="text-large" />}>
+          <DropdownItem key="new_project" >
             New Project
           </DropdownItem>
 
-          <DropdownItem key="quick_search" shortcut="⌘K">
+          <DropdownItem key="quick_search" >
             Quick search
           </DropdownItem>
           <DropdownItem
