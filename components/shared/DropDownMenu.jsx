46627beb71ed64ec61cacb8f7c236a1bcc97ad73
saman <sam.karimi727@gmail.com>
2025-10-22 04:30:50 +0330

    update DropDownMenu.jsx

--- components/shared/DropDownMenu.jsx
+++ components/shared/DropDownMenu.jsx
@@ -17,7 +17,7 @@ const DropMenu = () => {
         classNames={{
           base: "before:bg-default-200", // change arrow background
           content:
-            "py-1 px-1 border border-default-200 bg-linear-to-br from-white to-default-200 dark:from-default-50 dark:to-black",
+            "py-1 px-1 border border-default-200 bg-linear-to-br from-white to-default-200 dark:from-default-200 dark:to-black",
         }}>
         <DropdownTrigger>
           <Button variant="bordered"> menu</Button>
